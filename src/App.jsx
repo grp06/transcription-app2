7913dca24205f4518b0ae4757a3d5375f034d7c1
grp06 <gpickett00@gmail.com>
2023-04-21 11:07:38 -0500

    stable

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import io from 'socket.io-client'
 import RecordRTC from 'recordrtc'
 
@@ -8,20 +8,36 @@ const App = () => {
   const [isRecording, setIsRecording] = useState(false)
   const [transcription, setTranscription] = useState('')
   const [recorder, setRecorder] = useState(null)
+  const [totalAudioDuration, setTotalAudioDuration] = useState(0)
+  const [topic, setTopic] = useState('')
+  const [question, setQuestion] = useState('')
 
   useEffect(() => {
+    console.log('Setting up socket event listeners')
     socket.on('transcription', (transcription) => {
-      console.log('🚀 ~ socket.on ~ transcription:', transcription)
+      console.log('received transcription')
       setTranscription(transcription.text)
     })
 
+    socket.on('topic', (topic) => {
+      console.log('topic received:', topic) // Update this line
+      setTopic(topic)
+    })
+
+    socket.on('question', (question) => {
+      console.log('question received:', question) // Update this line
+      setQuestion(question)
+    })
+
     return () => {
+      console.log('Removing socket event listeners')
       socket.off('transcription')
+      socket.off('topic')
+      socket.off('question')
     }
   }, [])
 
   const startRecording = () => {
-    console.log('starting recording')
     setIsRecording(true)
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
       const options = {
@@ -30,9 +46,21 @@ const App = () => {
         numberOfAudioChannels: 1,
         recorderType: RecordRTC.StereoAudioRecorder,
         checkForInactiveTracks: true,
-        timeSlice: 5000,
+        timeSlice: 10000,
         ondataavailable: (blob) => {
-          socket.emit('audio', { buffer: blob })
+          const overlapDuration = 1000
+          const startTime = totalAudioDuration - overlapDuration
+          const endTime = startTime + options.timeSlice
+
+          setTotalAudioDuration(
+            totalAudioDuration + options.timeSlice - overlapDuration,
+          )
+
+          socket.emit('audio', {
+            buffer: blob,
+            start_time: startTime,
+            end_time: endTime,
+          })
         },
       }
 
@@ -46,24 +74,35 @@ const App = () => {
     setIsRecording(false)
     recorder.stopRecording(() => {
       socket.off('transcription')
+      socket.off('topic')
       recorder.clearRecordedData()
       setRecorder(null)
     })
   }
 
-  socket.on('connect', () => {
-    console.log('connected to socket server')
-  })
-
   return (
     <div>
-      <button onClick={startRecording} disabled={isRecording}>
-        Start Recording
-      </button>
-      <button onClick={stopRecording} disabled={!isRecording}>
-        Stop Recording
-      </button>
-      <div>{transcription}</div>
+      <div className="ratings-container">
+        <ul>
+          <li>
+            Topic: <span>{topic}</span>
+          </li>
+          <li>
+            Q: <span>{question}</span>
+          </li>
+        </ul>
+      </div>
+      {!isRecording && (
+        <button onClick={startRecording} disabled={isRecording}>
+          Start Recording
+        </button>
+      )}
+      {isRecording && (
+        <button onClick={stopRecording} disabled={!isRecording}>
+          Stop Recording
+        </button>
+      )}
+      <div className="transcription-container">{transcription}</div>
     </div>
   )
 }
